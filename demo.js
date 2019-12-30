0824889461c74145e83eb7870397f6ef6fb6963c
xindong dou <douxd@lifevc.com>
2019-12-30 19:03:37 +0800

    a

--- demo.js
+++ demo.js
@@ -186,8 +186,7 @@ function goPay(paywaycode, paydata) {
             }
             if (isiOS) {
                 //调用原生方法:微信支付
-
-                OCmodel.WeChatPay(paydata);
+                iosDo('WeChatPay',paydata);
             }
         }
     } catch (error) {
