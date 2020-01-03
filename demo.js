c81b16327f44a25241204481b7c3c7daf6614663
xindong dou <douxd@lifevc.com>
2020-01-03 19:11:44 +0800

    a

--- demo.js
+++ demo.js
@@ -102,10 +102,12 @@ function initApp() {
         log("initApp error");
         log(error);
     }
-    setTimeout(function () {
-        window.location.href = 'http://w2.lifevc.com/oneyearact/fulika.html?actcode=oneyeartest2&webid=35dd8c328d70445e83f216b79ef5e258';
-    },1000);
 
+
+}
+function jump() {
+    alert('jup')
+    window.location.href = 'http://w2.lifevc.com/oneyearact/fulika.html?actcode=oneyeartest2&webid=35dd8c328d70445e83f216b79ef5e258';
 }
 //设置原生app的头名称
 function setInnerTitle(title) {
