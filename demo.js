317c54ded42be64460e319a45d36d3cc379eca91
xindong dou <douxd@lifevc.com>
2020-01-15 18:59:14 +0800

    a

--- demo.js
+++ demo.js
@@ -241,11 +241,25 @@ function savePhotoToAlbum(imgbase64str) {
             //调用原生方法:弹出保存图片
             // iosDo('savePhotoToAlbum',imgbase64str);
             OCmodel.savePhotoToAlbum(imgbase64str)
+            // saveFile(imgbase64str,'file')
         }
     } catch (error) {
         log("savePhotoToAlbum error");
         log(error);
     }
+}/**
+ * 在本地进行文件保存
+ * @param  {String} data     要保存到本地的图片数据
+ * @param  {String} filename 文件名
+ */
+function saveFile(data, filename) {
+    const save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+    save_link.href = data;
+    save_link.download = filename;
+
+    const event = document.createEvent('MouseEvents');
+    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    save_link.dispatchEvent(event);
 }
 //呼出相册
 function callAlbum() {
