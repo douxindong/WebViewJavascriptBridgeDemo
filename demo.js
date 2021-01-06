e12dc25b3c2295a220151a85257c001abd666e66
xindong dou <douxd@lifevc.com>
2021-01-06 16:23:31 +0800

    a

--- demo.js
+++ demo.js
@@ -87,9 +87,34 @@ function iosDo(event,param){
     log("c");
     return null;
 }
+function downloadIamge(imgsrc, name) {//下载图片地址和图片名
+    let image = new Image();
+    // 解决跨域 Canvas 污染问题
+    image.setAttribute("crossOrigin", "anonymous");
+    // image.onload = function() {
+    //     let canvas = document.createElement("canvas");
+    //     canvas.width = image.width;
+    //     canvas.height = image.height;
+    //     let context = canvas.getContext("2d");
+    //     context.drawImage(image, 0, 0, image.width, image.height);
+    //     let url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+    //     let a = document.createElement("a"); // 生成一个a元素
+    //     let event = new MouseEvent("click"); // 创建一个单击事件
+    //     a.download = name || "photo"; // 设置图片名称
+    //     a.href = url; // 将生成的URL设置为a.href属性
+    //     a.dispatchEvent(event); // 触发a的单击事件
+    // };
+    image.src = imgsrc;
+}
 //初始化
 function initApp() {
     log("begin initApp ");
+    var i = 0
+    while (i<1000){
+        i++
+        console.log(i)
+        downloadIamge('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F01%2F59%2F32%2F015748571fb4bd6.jpg&refer=http%3A%2F%2Fku.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612512962&t=225518b3c63ca4057a475eca64dbc93e'+'&x='+i,'name')
+    }
     try {
         if (isAndroid) {
             window.OCmodel.getAppInfo(); //调用原生方法
